e742f945479773f116c1dcd5e232290e8c3f361e
Treasure <treasureaj14@gmail.com>
2026-08-10 12:41:30 +0100

    Insert into divisions table script

---
+++ migrations/009_seed_divisions.sql
@@ -0,0 +1,8 @@
+-- Inserting all the divisions into the division table
+INSERT INTO divisions (division_name, division_code) VALUES
+    ('Meridian Retail and Consumer', 'RETAIL'),
+    ('Concord Logistics', 'LOGISTICS'),
+    ('Veridian Financial Services', 'VFS'),
+    ('AgriCore', 'AGRI'),
+    ('Veridian Properties', 'PROPERTIES'),
+    ('Group Functions', 'GROUP');
